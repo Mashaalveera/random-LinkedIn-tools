4ae31c93d55bfaca227549dd43c593420962fffd
Andre Bradshaw <bradshaw.andre@gmail.com>
2018-12-25 20:19:37 -0500

    Update autoLike.js

--- autoLike.js
+++ autoLike.js
@@ -1,14 +1,12 @@
 var doc = document;
-
 var socialBar = doc.getElementsByClassName('feed-shared-social-actions feed-shared-social-action-bar ember-view');
-
 function getJobPostingObj(obj, n) {
       setTimeout(() => {
         doc.getElementById(obj.getAttribute('id')).scrollIntoView();
         var likeBtn = obj.getElementsByTagName('button')[0];
 		var btnStatus = obj.getElementsByClassName('visually-hidden')[0].innerText;
 		if(btnStatus == 'Like'){
-			likeBtn.click()
+			likeBtn.click();
 		}
       }, ((n + 1) * 1800));
     }
