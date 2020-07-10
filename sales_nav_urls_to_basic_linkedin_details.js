6a7eed3378328eb6ed49de0653d0f988faccc863
Andre Bradshaw <bradshaw.andre@gmail.com>
2020-07-10 10:47:35 -0400

    Update sales_nav_urls_to_basic_linkedin_details.js

--- sales_nav_urls_to_basic_linkedin_details.js
+++ sales_nav_urls_to_basic_linkedin_details.js
@@ -1,12 +1,118 @@
 var reg = (o, n) => o ? o[n] : '';
-var cn = (o, s) => o ? o.getElementsByClassName(s) : console.log(o);
-var tn = (o, s) => o ? o.getElementsByTagName(s) : console.log(o);
-var gi = (o, s) => o ? o.getElementById(s) : console.log(o);
+var cn = (o, s) => o ? o.getElementsByClassName(s) : false;
+var tn = (o, s) => o ? o.getElementsByTagName(s) : false;
+var gi = (o, s) => o ? o.getElementById(s) : false;
 var rando = (n) => Math.round(Math.random() * n);
+
 var delay = (ms) => new Promise(res => setTimeout(res, ms));
+var ele = (t) => document.createElement(t); 
+var attr = (o, k, v) => o.setAttribute(k, v);
+var a = (l, r) => r.forEach(a => attr(l, a[0], a[1]));
+
 var tsvready = (s) => s ? s.replace(/\t|\u0009/g, ' ').replace(/\r|\n/g, '↵').replace(/"/g, "'") : s;
 var reChar = (s) => typeof s == 'string' && s.match(/&#\d+;/g) && s.match(/&#\d+;/g).length > 0 ? s.match(/&#\d+;/g).map(el => [el, String.fromCharCode(reg(/(?<=&#).+?(?=;)/.exec(el),0))]).map(m => s = s.replace(new RegExp(m[0], 'i'), m[1])).pop() : s;
 var fixNameCase = (s) => s.split(/(?=[^áàâäãåÁÀÂÄÃæéèêëÉÈÊËíìîïñÑóòôöõøœÓÒÔÖÕØŒßÚÙÛÜúùûüa-zA-Z])\b/).map(el=> el.replace(/\w\S*/g, txt=> txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase())).join('').replace(/(?<=\bMc)\w/ig, t=> t.charAt(0).toUpperCase());
+
+
+var svgs = {
+    close: `<svg x="0px" y="0px" viewBox="0 0 100 100"><g style="transform: scale(0.85, 0.85)" stroke-width="1" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"><g transform="translate(2, 2)" stroke="#e21212" stroke-width="8"><path d="M47.806834,19.6743435 L47.806834,77.2743435" transform="translate(49, 50) rotate(225) translate(-49, -50) "/><path d="M76.6237986,48.48 L19.0237986,48.48" transform="translate(49, 50) rotate(225) translate(-49, -50) "/></g></g></svg>`,
+};
+  
+  function aninCloseBtn() {
+    var l1 = tn(this, 'path')[0];
+    var l2 = tn(this, 'path')[1];
+    l1.style.transform = "translate(49px, 50px) rotate(45deg) translate(-49px, -50px)";
+    l1.style.transition = "all 233ms";
+    l2.style.transform = "translate(49px, 50px) rotate(135deg) translate(-49px, -50px)";
+    l2.style.transition = "all 233ms";
+  }
+  
+  function anoutCloseBtn() {
+    var l1 = tn(this, 'path')[0];
+    var l2 = tn(this, 'path')[1];
+    l1.style.transform = "translate(49px, 50px) rotate(225deg) translate(-49px, -50px)";
+    l1.style.transition = "all 233ms";
+    l2.style.transform = "translate(49px, 50px) rotate(225deg) translate(-49px, -50px)";
+    l2.style.transition = "all 233ms";
+  }
+  
+  function dragElement() {
+    var el = this.parentElement;
+    var pos1 = 0,
+      pos2 = 0,
+      pos3 = 0,
+      pos4 = 0;
+    if (document.getElementById(this.id)) document.getElementById(this.id).onmousedown = dragMouseDown;
+    else this.onmousedown = dragMouseDown;
+  
+    function dragMouseDown(e) {
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      document.onmouseup = closeDragElement;
+      document.onmousemove = elementDrag;
+    }
+  
+    function elementDrag(e) {
+      pos1 = pos3 - e.clientX;
+      pos2 = pos4 - e.clientY;
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      el.style.top = (el.offsetTop - pos2) + "px";
+      el.style.left = (el.offsetLeft - pos1) + "px";
+      el.style.opacity = "0.85";
+      el.style.transition = "opacity 700ms";
+    }
+  
+    function closeDragElement() {
+      document.onmouseup = null;
+      document.onmousemove = null;
+      el.style.opacity = "1";
+    }
+  }
+  
+  
+  async function createUploadHTML(form_name){
+    var cont_id = 'form_upload_elm';
+    if(gi(document,cont_id)) gi(document,cont_id).outerHTML = '';
+    var rect = document.body.getBoundingClientRect();
+    var cont = ele('div');
+    a(cont,[['id',cont_id],['style', `padding: 0; position: fixed; top: ${rect.top+15}px; left: ${rect.left+35}px; z-index: ${new Date().getTime()}; width: ${rect.width*0.8}px; border: 1px solid #004471; border-radius: 0.4em; background: rgba(5, 37, 51, 0.6);`]]);
+    document.body.appendChild(cont);
+  
+    var head = ele('div');
+    a(head, [['style', `display: grid; grid-template-columns: 1fr 29px; width: 100%; background: #0a1114; border: 1.6px solid #0a1114; border-top-left-radius: 0.4em; border-top-right-radius: 0.4em; cursor: move;`]]);
+    cont.appendChild(head);
+    head.onmouseover = dragElement;
+  
+    var txt = ele('div');
+    a(txt, [['style', `color: #fff; font-size: 1.3em; border-radius: 0.5em; padding: 4px;`]]);
+    head.appendChild(txt);
+    txt.innerText = form_name;
+  
+    var cls = ele('div');
+    a(cls, [['style', `width: 27px; height: 27px; cursor: pointer;`]]);
+    head.appendChild(cls);
+    cls.innerHTML = svgs.close;
+    cls.onmouseenter = aninCloseBtn;
+    cls.onmouseleave = anoutCloseBtn;
+    cls.onclick = () => cont.outerHTML = '';
+  
+    var cbod = ele('div');
+    a(cbod,[['style',`background: #0a1114; display: grid; grid-template-rows: auto; grid-gap: 6px; padding: 2px; max-height: ${(screen.availHeight*0.75)}px; overflow-y: auto;`]]);
+    cont.appendChild(cbod);
+    
+    
+    var textarea = ele('textarea');
+    a(textarea,[['id', 'url_list_from_user_input'],['style',`height: 300px; padding: 6px; border-radius: 0.4em; background: #f1f1f1; color: #004471;`]]);
+    cbod.appendChild(textarea);
+
+    var btn = ele('div');
+    a(btn,[['style',`padding: 6px; border-radius: 0.4em; background: #004471; color: #f1f1f1; cursor: pointer; text-align: center;`]]);
+    cbod.appendChild(btn);
+    btn.innerText = 'Convert URLs';
+    btn.onclick = runConverter;
+}  
+
 
 function convert2TsvAndDownload(records, named_file){
     var fileArray = records;
@@ -174,8 +280,8 @@ function parseBasics(obj) {
       }
     }
 
-async function profileByPath(pubPath,credentials) {
-  var text = await handleFetch("https://www.linkedin.com/voyager/api/identity/profiles/" + pubPath + "/profileActions",creds(credentials), 'text');
+async function profileByPath(obj,credentials) {
+  var text = await handleFetch(`https://www.linkedin.com/voyager/api/identity/profiles/${obj.target_path}/profileActions`,creds(credentials), 'text');
   var doc = new DOMParser().parseFromString(text, 'text/html');
   try {
     var j = JSON.parse(doc.body.innerText);
@@ -194,9 +300,8 @@ async function getBasics(char39,credentials) {
 }
 
 
-async function getBasicProfileFromSalesNavLink(url,credentials){
-  var parseSalesNavLink = (url) => reg(/(?<=\/people\/).{39}/.exec(url),0);
-  var char39 = await profileByPath(parseSalesNavLink(url),credentials);
+async function getBasicProfileFromSalesNavLink(obj,credentials){
+  var char39 = await profileByPath(obj,credentials);
   if(char39){
     var basics = await getBasics(char39,credentials);
     return basics;
@@ -209,32 +314,47 @@ async function loopThroughSalesNavLinks(arr){
   var contain_arr = [];
   var credentials = await getCredentials();
   for(var i=0; i<arr.length; i++){
-    var profile = await getBasicProfileFromSalesNavLink(arr[i],credentials);
-    await delay(rando(1400)+1222);
-    if(profile && profile.download_now != true){
-      profile['sales_nav'] = arr[i];
-      contain_arr.push(profile);
+    if(arr[i].run){
+      var profile = await getBasicProfileFromSalesNavLink(arr[i],credentials);
+      await delay(rando(1400)+1222);
+      if(profile && profile.download_now != true){
+        var merged = {...arr[i],...profile};
+        contain_arr.push(merged);
+      }else{
+        break;
+      }
     }else{
-      break;
+        contain_arr.push(arr[i]);
     }
   }
   return contain_arr;
 }
 
-async function initSaleNavConverter(all_urls){
-  var arr = all_urls.split(/\n/).map(r=> r.trim()).filter(r=> r);
-  var all_profiles = await loopThroughSalesNavLinks(arr);
+async function initSaleNavConverter(query_objects){
+  var all_profiles = await loopThroughSalesNavLinks(query_objects);
   console.log(all_profiles);
   convert2TsvAndDownload(all_profiles,'converted_profiles.tsv');
+
 }
 
-/*
-DROP ALL OF YOUR URLS in the "all_urls" variable. Run this in the dev console.
-*/
 
-var all_urls = `https://www.linkedin.com/sales/people/ACwAAAA0NiQBtrOQhXlqwO2sCz7WHxVW23I-k3M,name,CFdk
-https://www.linkedin.com/sales/people/ACwAAAA0NiQBtrOQhXlqwO2sCz7WHxVW23I-k3M,name,CFdk
-https://www.linkedin.com/sales/people/ACwAAAA0NiQBtrOQhXlqwO2sCz7WHxVW23I-k3M,name,CFdk
-https://www.linkedin.com/sales/people/ACwAAAA0NiQBtrOQhXlqwO2sCz7WHxVW23I-k3M,name,CFdk`;
 
-initSaleNavConverter(all_urls)
+var linkedinType = (s) => s && s.length ? s.map(url=> 
+        /recruiter\/profile\/\d+,\w+,\w+/.test(url)
+            ? {run: false, target_path: reg(/(?<=recruiter\/profile\/)\d+,\w+,\w+/.exec(url),0),url: url}
+            : {run: true, target_path: reg(/(?<=\/in\/|\/recruiter\/profile\/|\/sales\/people\/).+?(?=,|\/|\s|$)/.exec(url),0),url: url}
+) : [];
+
+function runConverter(){
+  var unqTargetpath = (a, o) => a.filter(i => o.hasOwnProperty(i.target_path) ? false : (o[i.target_path] = true));
+  var textarea = gi(document,'url_list_from_user_input');
+  if(textarea.value && /linkedin\.com\/(in|recruiter|sales)/.test(textarea.value)){
+    var paths = textarea.value.match(/linkedin\.com\/(in|recruiter\/profile|sales\/people)\/\S+/g);
+    var convert_these = unqTargetpath(linkedinType(paths),{});
+
+    initSaleNavConverter(convert_these)
+  }
+}
+
+
+createUploadHTML('LinkedIn URL converter');
